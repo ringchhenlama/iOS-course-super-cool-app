6b56d18b25797c7a4ba75ac4db7ece2a3782b189
Ringchhen's Mac <ringchhensmac@ringchhenss-mac.local>
2016-05-27 05:07:37 -0700

    Initial Commit

---
+++ SuperCoolTwo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCoolTwo
+//
+//  Created by Ringchhen's Mac on 5/27/16.
+//  Copyright © 2016 Ringchhen's Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
